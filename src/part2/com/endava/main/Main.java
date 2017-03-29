94ca57f1389d538e44aa0329d64b430254506f94
Stason1o <stasikus1996@gmail.com>
2017-03-29 10:18:31 +0300

    finish part two.
    Remove User

--- src/part2/com/endava/main/Main.java
+++ src/part2/com/endava/main/Main.java
@@ -1,26 +1,24 @@
 package part2.com.endava.main;
 
 
-import part2.com.endava.entities.User;
 import part2.com.endava.util.SimpleLRU;
-
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by sbogdanschi on 28/03/2017.
  */
 public class Main {
     public static void main(String... args) {
-        SimpleLRU<LocalDateTime, User> simpleLRU = new SimpleLRU<>(5);
+        SimpleLRU<String, LocalDateTime> simpleLRU = new SimpleLRU<>(50);
+        List<String> nameList = new ArrayList<>();
+        for(int i = 0; i < 100; i++)
+            nameList.add("User" + i);
 
-        simpleLRU.put(LocalDateTime.now(), new User("Stas", "Stas", 21));
-        simpleLRU.put(LocalDateTime.of(2017, 12, 13, 12, 56), new User("qwer", "eqrfvwer", 12));
-        simpleLRU.put(LocalDateTime.of(2000, 12, 13, 12, 10), new User("jhrfgv", "sfhmgv", 43));
-        simpleLRU.put(LocalDateTime.now(), new User("jyrnhbtgfv", "wefwr", 14));
-        simpleLRU.put(LocalDateTime.of(2004, 10, 14, 23, 54), new User("etrgvwer", "trgv2er", 23));
+        for(int i = 0; i < 3000; i++)
+            simpleLRU.put(nameList.get((int) (Math.random() * 100) ), LocalDateTime.now());
 
-        simpleLRU.put(LocalDateTime.now(), new User("new UIser", "q3rwefe", 11));
-
-        System.out.println(simpleLRU);
+        simpleLRU.entrySet().forEach( item-> System.out.println(item.getKey() + " " + item.getValue()));
     }
 }
