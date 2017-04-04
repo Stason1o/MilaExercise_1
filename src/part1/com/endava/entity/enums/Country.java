7f0257977c385c88b909b81a9ed930a1607aab9a
Stason1o <stasikus1996@gmail.com>
2017-04-04 11:48:58 +0300

    Add enum Country

---
+++ src/part1/com/endava/entity/enums/Country.java
@@ -0,0 +1,37 @@
+package part1.com.endava.entity.enums;
+
+/**
+ * Created by sbogdanschi on 4/04/2017.
+ */
+public enum Country {
+    USA,
+    MOLDOVA,
+    LONDON,
+    PARIS,
+    MONACO,
+    FRANCE,
+    MEXICO,
+    SINGAPORE,
+    BELGIUM,
+    SPAIN,
+    BANGLADESH,
+    BRAZIL,
+    ALGIR,
+    INDIA,
+    IZRAEL,
+    IRELAND,
+    IEMEN,
+    KAMERUN,
+    CANADA,
+    CHINA,
+    ITALY,
+    KUVEIT,
+    CUBA,
+    LATVIA,
+    LUXEMBURG,
+    MADAGASKAR,
+    NIGER,
+    NIGERIA
+
+
+}
